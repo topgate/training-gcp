d4638be4d69f3e799be74e619938870c0303a151
Shingo Ishimura <metal.tie@gmail.com>
2017-03-27 22:24:12 +0900

    BackendServiceをglobalにした

--- CPO200/http-lb/create.sh
+++ CPO200/http-lb/create.sh
@@ -12,42 +12,46 @@ gcloud compute instance-groups managed create "web-group-us-c" --zone "us-centra
 
 gcloud compute http-health-checks create basic-check
 
-gcloud compute backend-services create web-service --protocol HTTP --http-health-checks basic-check
+gcloud compute backend-services create web-service --protocol HTTP --http-health-checks basic-check --global
 
 gcloud compute backend-services add-backend web-service \
     --balancing-mode UTILIZATION \
     --max-utilization 0.8 \
     --capacity-scaler 1 \
     --instance-group web-group-us-b \
-    --instance-group-zone us-central1-b
+    --instance-group-zone us-central1-b \
+    --global
 
 gcloud compute backend-services add-backend web-service \
     --balancing-mode UTILIZATION \
     --max-utilization 0.8 \
     --capacity-scaler 1 \
     --instance-group web-group-us-c \
-    --instance-group-zone us-central1-c
+    --instance-group-zone us-central1-c \
+    --global
 
 gcloud compute instance-templates create "video-template" --machine-type "f1-micro" --network "default" --metadata "startup-script-url=gs://cpo200demo1.appspot.com/startup.sh" --tags "http-server" --image "/debian-cloud/debian-8-jessie-v20161027"
 
 gcloud compute instance-groups managed create "video-group-us-b" --zone "us-central1-b" --base-instance-name "video-group-us-b" --template "web-template" --size "2"
 gcloud compute instance-groups managed create "video-group-us-c" --zone "us-central1-c" --base-instance-name "video-group-us-b" --template "web-template" --size "2"
 
-gcloud compute backend-services create video-service --protocol HTTP --http-health-checks basic-check
+gcloud compute backend-services create video-service --protocol HTTP --http-health-checks basic-check --global
 
 gcloud compute backend-services add-backend video-service \
     --balancing-mode UTILIZATION \
     --max-utilization 0.8 \
     --capacity-scaler 1 \
     --instance-group video-group-us-b \
-    --instance-group-zone us-central1-b
+    --instance-group-zone us-central1-b \
+    --global
 
 gcloud compute backend-services add-backend video-service \
     --balancing-mode UTILIZATION \
     --max-utilization 0.8 \
     --capacity-scaler 1 \
     --instance-group video-group-us-c \
-    --instance-group-zone us-central1-c
+    --instance-group-zone us-central1-c \
+    --global
 
 gcloud compute url-maps create web-map --default-service web-service
 
